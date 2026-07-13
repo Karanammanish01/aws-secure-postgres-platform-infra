d8792354957c9744feed65b99f15df7c05663f74
Manish Prod <manishkaranam20@gmail.com>
2026-07-13 23:53:13 +0530

    IAM policy and Instance profile updated

--- environments/dev/outputs.tf
+++ environments/dev/outputs.tf
@@ -13,3 +13,7 @@ output "security_group_id" {
 output "security_group_arn" {
   value = module.secuirty.security_group_arn
 }
+
+output "aws_iam_instance_profile_arn" {
+  value = module.iam_role_policy.instance_profile_arn
+}
